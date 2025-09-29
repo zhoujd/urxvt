830a8b62cc84531dd6778e797414be61dd34c0a5
Zachary Zhou <zchrzhou@gmail.com>
2025-09-29 14:41:29 +0800

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+build() {
+    mkdir -p rxvt-unicode/build
+    pushd rxvt-unicode/build
+    ../configure --prefix=/usr/local --enable-everything
+    make -j4
+    sudo make install
+    popd
+    echo "Build Done"
+}
+
+clean() {
+    git clean -dfx
+    echo "clean Done"
+}
+
+case $1 in
+    build|-b )
+        build
+        ;;
+    clean|-c )
+        clean
+        ;;
+    * )
+        echo "Usage $(basename $0) {build|-b|clean|-c}"
+        ;;
+esac
